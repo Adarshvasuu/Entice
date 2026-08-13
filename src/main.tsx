59b768553a1702cb5c68ef884ecbbcdc069fd8f6
Adarshvasuu <vasuadarsh52@gmail.com>
2026-08-13 12:04:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './index.css'; // Make sure this matches your CSS file name
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <App />);
-} else {
-  createRoot(rootElement).render(<App />);
-}
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
